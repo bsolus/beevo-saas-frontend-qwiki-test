6991b01b3983fb46a529f73b29d5b9a844482aa2
dfernandesbsolus <96054351+dfernandesbsolus@users.noreply.github.com>
2024-11-15 13:08:01 +0100

    add test

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { qwikCity } from '@builder.io/qwik-city/vite';
+import { qwikVite } from '@builder.io/qwik/optimizer';
+import { defineConfig, loadEnv } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig(async (config) => {
+	return {
+		// Enable to analyze via source-map-explorer
+		ssr: { target: 'webworker' },
+		build: {
+			sourcemap: config.mode === 'development',
+		},
+		plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+		preview: {
+			headers: {
+				'Cache-Control': 'public, max-age=600',
+			},
+		},
+	};
+});
